31e60e20583a378ead9004ec15286dd553463bd9
nickhallph <nick.hall@yum.com>
2018-01-30 10:26:57 -0600

    continuing fixing eslint errs, not proud of todoReducer.js changes

--- client/components/todo.js
+++ client/components/todo.js
@@ -1,33 +1,33 @@
-import React from "react";
+import React from 'react';
 
 export default class TodoComp extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.onTodoSubmit = this.onTodoSubmit.bind(this);
-	}
+    this.onTodoSubmit = this.onTodoSubmit.bind(this);
+  }
 
-	onTodoSubmit(e) {
-		const value = this.input.value;
-		const id = Math.floor((1 + Math.random()) * 0x10000);
-		this.props.onTodoSubmit(e, value, id);
-		this.input.value = '';
-	}
+  onTodoSubmit(e) {
+    const value = this.input.value;
+    const id = Math.floor((1 + Math.random()) * 0x10000);
+    this.props.onTodoSubmit(e, value, id);
+    this.input.value = '';
+  }
 
-	render() {
-		return (
-			<section className="todo-container">
-				<h1>Insert your todo below</h1>
-				<form onSubmit={this.onTodoSubmit}>
-					<input
-						type="text"
-						placeholder="Enter todo.."
-						autoFocus={true}
-						ref={(ref) => { this.input = ref; }}
-					/>
-					<button type="submit">Add</button>
-				</form>
-			</section>
-		)
-	}
+  render() {
+    return (
+      <section className="todo-container">
+        <h1>Insert your todo below</h1>
+        <form onSubmit={this.onTodoSubmit}>
+          <input
+            type="text"
+            placeholder="Enter todo.."
+            autoFocus
+            ref={(ref) => { this.input = ref; }}
+          />
+          <button type="submit">Add</button>
+        </form>
+      </section>
+    );
+  }
 }
--- client/reducers/todoReducer.js
+++ client/reducers/todoReducer.js
@@ -1,75 +1,79 @@
+/* eslint-disable no-case-declarations */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-shadow */
+/* eslint-disable no-param-reassign */
+
+// ^^ NOT PROUD!
 import * as types from '../actionTypes';
 
 const initialState = {
   todos: []
-}
+};
 
 const reducer = (state = initialState, action) => {
-  switch(action.type) {
+  switch (action.type) {
     // Add Todo
     case types.ADD_TODO:
-      const todo = state.todos.push(
-        Object.assign({}, {
-          id: action.payload.id,
-          todo: action.payload.todo,
-          completed: false,
-          isEditing: false,
-        })
-      );
+      const { id, todo } = action.payload;
 
-      return {
-        ...state
-      }
+      const x = state.todos.push({
+        id,
+        todo,
+        completed: false,
+        isEditing: false
+      });
 
-    // Delete Todo
+      return { ...state };
+
+    // Delete TodO
     case types.DELETE_TODO:
       return {
         ...state,
         todos: state.todos.filter(v => v.id !== action.payload)
-      }
+      };
 
     // Edit Todo (Activate Edit Mode)
     case types.EDIT_TODO:
       return {
         ...state,
-        todos: state.todos.map(todo => {
-          if(action.payload === todo.id) {
+        todos: state.todos.map((todo) => {
+          if (action.payload === todo.id) {
             todo.isEditing = !todo.isEditing;
             return todo;
-           }
-           return todo;
+          }
+          return todo;
         })
-      }
+      };
 
     // Change Todo (Actually Editing Todo Value)
     case types.CHANGE_TODO:
-    return {
-      ...state,
-      todos: state.todos.map(todo => {
-        if(action.payload.todo === todo.id) {
-          todo.todo = action.payload.newValue;
+      return {
+        ...state,
+        todos: state.todos.map((todo) => {
+          if (action.payload.todo === todo.id) {
+            todo.todo = action.payload.newValue;
+            return todo;
+          }
           return todo;
-        }
-        return todo;
-      })
-    }
+        })
+      };
 
     // Mark Todo As completed
     case types.MARK_COMPLETED:
       return {
         ...state,
-        todos: state.todos.map(todo => {
-          if(todo.id === action.payload) {
+        todos: state.todos.map((todo) => {
+          if (todo.id === action.payload) {
             todo.completed = !todo.completed;
             return todo;
           }
           return todo;
         })
-      }
+      };
 
     default:
-      return { ...state }
+      return { ...state };
   }
-}
+};
 
 export default reducer;
