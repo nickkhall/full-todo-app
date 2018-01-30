630e1053ee9fc79c7eefedde08ecb055473836d7
nickhallph <nick.hall@yum.com>
2018-01-30 11:26:22 -0600

    finished fixing eslint errors

--- client/containers/Routes.js
+++ client/containers/Routes.js
@@ -1,16 +1,14 @@
-import React from 'react'
-import { browserHistory, HashRouter, Route, Switch } from 'react-router-dom'
+import React from 'react';
+import { browserHistory, HashRouter, Route, Switch } from 'react-router-dom';
 
 import Todo from './Todo';
 
-const Routes = () => {
-    return (
-    	<HashRouter history={ browserHistory }>
-	        <Switch>
-	        	<Route exact={ true } path="/" component={ Todo }/>
-	        </Switch>
-        </HashRouter>
-    );
-}
+const Routes = () => (
+  <HashRouter history={browserHistory}>
+    <Switch>
+      <Route exact path="/" component={Todo} />
+    </Switch>
+  </HashRouter>
+);
 
-export default Routes
+export default Routes;
--- client/store.js
+++ client/store.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import { createStore, applyMiddleware } from 'redux'
+/* eslint-disable no-underscore-dangle */
+import { createStore } from 'redux';
 
 import reducer from './reducers';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
