f9e6894f299b9d7b49d58eedd993924539e4d967
nickhallph <nick.hall@yum.com>
2018-01-30 10:03:51 -0600

    implemented eslint

--- client/components/todoList.js
+++ client/components/todoList.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 
-export default class TodoCompList extends Component {
+export default class TodoCompList extends React.Component {
   constructor(props) {
     super(props);
 
